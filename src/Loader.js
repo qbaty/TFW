8344647016dc16cc9487e775779449aee65077ad
qbaty <qbaty.qi@gmail.com>
2013-07-16 10:43:06 +0800

    Create Loader.js

---
+++ src/Loader.js
@@ -0,0 +1,18 @@
+/*
+** Loader by qbatyqi 2013-7-12
+** contain load css
+** contain load js
+** to-do load img
+*/
+var Loader = {
+  loadJS : function(path, callback){
+  	//TO-DO
+		
+	},
+	loadCSS : function(){
+		//TO-DO
+	},
+	loadImage : function(){
+		//TO-DO
+	}
+};
